0e4153e1e88a5f8c8042c98b8601f98ade40d09e
Hozen Li <Hozen@live.com>
2020-09-07 10:17:33 +0800

    增加座右铭

--- script/main.js
+++ script/main.js
@@ -11,6 +11,8 @@ function mian() {
     mottos: [
       '不积跬步无以至千里', '北海虽赊，扶摇可接', '欲穷千里目，更上一层楼!', '大鹏一日同风起，扶摇直上九万里',
       '会当凌绝顶，一览众山小', '驽马十驾，功在不舍', '一寸光阴一寸金', '黄沙百战穿金甲，不破楼兰终不还',
+      '故天将降大任于是人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能',
+      '鹏北海，凤朝阳。又携书剑路茫茫。明年此日青云去，却笑人间举子忙',
     ],
   };
 
@@ -62,7 +64,7 @@ function formatNumber(number, length) {
 }
 
 function getDeadline() {
-  return new Date(2019, 11, 22);
+  return new Date(2020, 11, 26);
 }
 
 function getTimerNumbers(deadline) {
